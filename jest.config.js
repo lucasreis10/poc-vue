21801de819ae8fb4c4f3b039251fba85aeb47e79
lucasreisc1 <lucas.reis@involves.com>
2022-03-14 11:41:03 -0300

    config(tests): add coverage tests

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,13 @@
 module.exports = {
   preset: '@vue/cli-plugin-unit-jest/presets/no-babel',
+  collectCoverage: true,
+  collectCoverageFrom: [
+    'src/**/*.{js,vue}',
+    '!src/main.js', // No need to cover bootstrap file
+  ],
+  coverageThreshold: {
+    global: {
+      lines: 50,
+    },
+  },
 };
